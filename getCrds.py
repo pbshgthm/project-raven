c7bacaa40b6512d282c421ec2a8a2833c962cf4a
wanderling <poobeshgowtham@live.com>
2019-06-20 02:00:08 +0530

    handle-missing-key

--- getCrds.py
+++ getCrds.py
@@ -6,6 +6,12 @@ responseattributes=none&locationattributes=none%2Car&gen=9\
 &app_id=oauTasEavzggDxpgTOJD&app_code=ssniJWlEDlNtElcjMBgjtw&searchtext='
 
 
+
+api_url="https://geocoder.api.here.com/6.2/geocode.json?\
+responseattributes=none&locationattributes=none%2Car&gen=9\
+&app_id=oauTasEavzggDxpgTOJD&app_code=ssniJWlEDlNtElcjMBgjtw&country=IND\
+&state="
+
 with open('master_data.json') as file:
     data=json.loads(file.read())
 
@@ -15,6 +21,7 @@ print(len(data))
 
 def extract_loc(place,name):
     
+    print(place,name)
     geo=place['Location']['DisplayPosition']
     crd=(geo['Longitude'],geo['Latitude'])
     meta=(place['MatchLevel'],place['Relevance'])
@@ -46,41 +53,67 @@ for i in data:
     print(child['id'])
 
    
-
-    q=child['native']['vil']+', '+child['native']['area']
-    req=requests.get(api_url+q).json()['Response']['View']
-    if len(req) ==0:
+    st=child['native']['area'].split(',')[1]
+    vl=child['native']['vil']
+    if st=="" or vl=="" or st==" " or vl==" ":
         child['native']['vil']=fill_blank(child['native']['vil'])
     else:
-        loc=extract_loc(req[0]["Result"][0],child['native']['vil'])
-        child['native']['vil']=loc
+        q=st+'&searchtext='+vl
+        print(q)
+        req=requests.get(api_url+q).json()['Response']['View']
+        if len(req) ==0:
+            child['native']['vil']=fill_blank(child['native']['vil'])
+        else:
+            loc=extract_loc(req[0]["Result"][0],child['native']['vil'])
+            child['native']['vil']=loc
 
 
-    q=child['native']['ps']+', '+child['native']['area']
-    req=requests.get(api_url+q).json()['Response']['View']
-    if len(req) ==0:
+    st=child['native']['area'].split(',')[1]
+    vl=child['native']['ps']
+    if st=="" or vl=="" or st==" " or vl==" ":
         child['native']['ps']=fill_blank(child['native']['ps'])
     else:
-        loc=extract_loc(req[0]["Result"][0],child['native']['ps'])
-        child['native']['ps']=loc
+        q=st+'&searchtext='+vl
+        print(q)
+        req=requests.get(api_url+q).json()['Response']['View']
+        if len(req) ==0:
+            child['native']['ps']=fill_blank(child['native']['ps'])
+        else:
+            loc=extract_loc(req[0]["Result"][0],child['native']['ps'])
+            child['native']['ps']=loc
 
 
-    q=child['raid']['vil']+', '+child['raid']['area']
-    req=requests.get(api_url+q).json()['Response']['View']
-    if len(req) ==0:
+    st=child['raid']['area'].split(',')[1]
+    vl=child['raid']['vil']
+    if st=="" or vl=="" or st==" " or vl==" ":
         child['raid']['vil']=fill_blank(child['raid']['vil'])
     else:
-        loc=extract_loc(req[0]["Result"][0],child['raid']['vil'])
-        child['raid']['vil']=loc
+        q=st+'&searchtext='+vl
+        print(q)
+        req=requests.get(api_url+q).json()['Response']['View']
+        if len(req) ==0:
+            child['raid']['vil']=fill_blank(child['raid']['vil'])
+        else:
+            loc=extract_loc(req[0]["Result"][0],child['raid']['vil'])
+            child['raid']['vil']=loc
 
 
-    q=child['raid']['ps']+', '+child['raid']['area']
-    req=requests.get(api_url+q).json()['Response']['View']
-    if len(req) ==0:
+    st=child['raid']['area'].split(',')[1]
+    vl=child['raid']['ps']
+    if st=="" or vl=="" or st==" " or vl==" ":
         child['raid']['ps']=fill_blank(child['raid']['ps'])
     else:
-        loc=extract_loc(req[0]["Result"][0],child['raid']['ps'])
-        child['raid']['ps']=loc
+        q=st+'&searchtext='+vl
+        print(q)
+        req=requests.get(api_url+q).json()['Response']['View']
+        if len(req) ==0:
+            child['raid']['ps']=fill_blank(child['raid']['ps'])
+        else:
+            loc=extract_loc(req[0]["Result"][0],child['raid']['ps'])
+            child['raid']['ps']=loc
+
+
+    
 
     
     
