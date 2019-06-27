dfe0282516cd0c30660e216216a52dbb34d2d347
wanderling <poobeshgowtham@live.com>
2019-06-27 16:28:06 +0530

    dotmaps

--- frontend/support.js
+++ frontend/support.js
@@ -7,7 +7,7 @@ var Data = {
 	},
 
 	toGeojson : function(crds,typ="Point"){
-      	geo={};
+      	var geo={};
       	geo['type']='FeatureCollection';
       	geo['features']=[];
         for(var i=0;i<crds.length;i++){
@@ -29,32 +29,107 @@ var Data = {
       },
 
 
-    getPoints : function(src){
+  getPoints : function(src){
     	src=src.split('_')
-    	crds=[]
+    	var crds=[]
     	for(var i=0;i<this.raw.length;i++){
     		crds.push(this.raw[i][src[0]][src[1]]['crd'])
     	}
     	return this.toGeojson(crds)
-    },
+  },
 
-    getLines : function(src,dst){
+  getLines : function(src,dst){
     	src=src.split('_')
     	dst=dst.split('_')
 
-    	crds=[]
+    	var crds=[]
     	for(var i=0;i<this.raw.length;i++){
     		crds.push([this.raw[i][src[0]][src[1]]['crd'],this.raw[i][dst[0]][dst[1]]['crd']])
     	}
     	return this.toGeojson(crds,"LineString")
-    },
+  },
 
-    ageDist : function(){
-    	var age=[]
+  ageDist : function(){
+    	var age=Array(21).fill(0)
+      var tot=0;
     	for(var i=0;i<this.raw.length;i++){
-    		age.push(this.raw[i]['age'])
+    		age[this.raw[i]['age']]+=1;
+        tot+=1
     	}
-    	return age.sort(function(a,b){return a-b});
-    }	
+      console.log(tot)
+      age.shift()
+    	return age;
+  },
+
+  traffickDate : function(){
+      var date=[];
+      var s_freq=[];
+      for(var i=0;i<this.raw.length;i++){
+        var since=this.raw[i]['since'];
+        var r_date=this.raw[i]['r_date'];
+        if(since==-1)continue;
+        if(r_date=="")continue;
+
+        r_date=r_date.split("/");
+        r_date=r_date[1]+"/"+r_date[0]+"/"+r_date[2];
+        var rr_date=new Date(r_date)
+        rr_date.setDate(rr_date.getDate()+(-1*since))
+
+        var dd = rr_date.getDate();
+        var mm = rr_date.getMonth() + 1;
+        var y = rr_date.getFullYear();
+
+        var t_date = dd + '/'+ mm + '/'+ y;
+        var t_mdate = (y-2009)*12+mm
+        
+        date.push([r_date,since,r_date]);
+
+        if(y>2009)
+        s_freq.push(t_mdate);
+
+      }
+      return s_freq;
+  },
+
+  industryDist : function(){
+      var ind_dict={}
+      for(var i=0;i<this.raw.length;i++){
+        var ind=this.raw[i]['indst'];
+        if(ind=="")continue;
+        var task=this.raw[i]['task'];
+
+        if(ind in ind_dict)
+          ind_dict[ind]+=1;
+        else
+          ind_dict[ind]=1;
+      }
+      ind_list=[];
+      for(i in ind_dict){
+        ind_list.push([i,ind_dict[i]])
+      }
+
+
+      return ind_list.sort(function (a,b){return b[1]-a[1]});
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
